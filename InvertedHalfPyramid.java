322a5f6bf3805d9eae509c53d294570697979655
Shashikant <shashikantnonstop@gmail.com>
2024-07-15 18:06:49 +0530

    Merge branch 'main' of

--- InvertedHalfPyramid.java
+++ InvertedHalfPyramid.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class HalfPyramid {
+public class InvertedHalfPyramid {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the height of the Pyramid : ");
         int l=in.nextInt();
 
-        for(int i=1;i<=l;i++){
+        for(int i=l;i>=1;i--){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
