7a95991fe2a58ecf56e39a62d069ca37dad175dc
Shashikant <shashikantnonstop@gmail.com>
2024-07-22 23:08:18 +0530

    Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -3,9 +3,9 @@ public class Rectangle {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the length of the rectangle : ");
+        System.out.print("Enter the length of the rectangle : ");
         int l=in.nextInt();
-        System.out.println("Enter the breadth of the rectangle : ");
+        System.out.print("Enter the breadth of the rectangle : ");
         int b=in.nextInt();
 
         for(int i=0;i<l;i++){
