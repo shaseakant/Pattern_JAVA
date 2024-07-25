9ae680c97bc3ca2c3f3f664cdf932ec489505485
Shashikant <shashikantnonstop@gmail.com>
2024-07-25 23:30:21 +0530

    InvertedHalfPyramid180

--- InvertedHalfPyramid180.java
+++ InvertedHalfPyramid180.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class InvertedHalfPyramid180 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the height of the Pyramid : ");
+        System.out.print("Enter the height of the Pyramid : ");
         int l=in.nextInt();
 
         for(int i=1;i<=l;i++){
